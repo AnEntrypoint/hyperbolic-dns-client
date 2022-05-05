fc86d270ec1b681bb6c536e2fde2efed90fae8c9
lanmower <almagestfraternite@gmail.com>
2022-05-05 14:04:05 +0200

    Update runnode.js

--- runnode.js
+++ runnode.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const node = require('./node.js');
 const sites = fs.readdirSync('sites');
+if(!fs.existsSync('run')) {
+   require('./init.js');
+   fs.closeSync(fs.openSync('run', 'w'));
+}
+
 for(const site of sites) {
    const file = fs.readFileSync('./sites/'+site+'/hyperconfig.json');
    const config = JSON.parse(file);

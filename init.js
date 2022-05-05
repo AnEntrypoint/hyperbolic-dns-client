687d7b1afc30ef4713a844025328fa6b941eed59
lanmower <almagestfraternite@gmail.com>
2022-05-05 12:47:44 +0200

    Update init.js

--- init.js
+++ init.js
@@ -8,23 +8,26 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Enter a unique private seed: ', function (password) {
-  rl.question('Enter your contact email: ', function (email) {
-    rl.question('Enter the destination address (e.g. https://example.org: ', function (address) {
-      const keyPair = crypto.keyPair(crypto.data(Buffer.from(password)));
-      const bkey = b32.encode(keyPair.publicKey).replace('====','').toLowerCase();
-      console.log('Address will be: ', bkey+".matic.ml");
-      fs.mkdirSync('sites/'+bkey+'/', { recursive: true }, (err) => {console.log(err)});
-      fs.writeFileSync('.env', 'KEY='+password);
-      fs.writeFileSync('sites/'+bkey+'/hyperconfig.json', JSON.stringify({key:password, target:address}));
-      fs.writeFileSync('sites/'+bkey+'/config.json', JSON.stringify({sites:[{subject:bkey+".matic.ml"}], maintainerEmail:email}));
-      fs.writeFileSync('address', bkey+".matic.ml");
-      rl.close();
+const args = process.argv.slice(2);
+if(args.length && args[0] == '-q') {
+  rl.question('Enter a unique private seed: ', function (password) {
+    rl.question('Enter your contact email: ', function (email) {
+      rl.question('Enter the destination address (e.g. https://example.org: ', function (address) {
+        const keyPair = crypto.keyPair(crypto.data(Buffer.from(password)));
+        const bkey = b32.encode(keyPair.publicKey).replace('====','').toLowerCase();
+        console.log('Address will be: ', bkey+".matic.ml");
+        fs.mkdirSync('sites/'+bkey+'/', { recursive: true }, (err) => {console.log(err)});
+        fs.writeFileSync('.env', 'KEY='+password);
+        fs.writeFileSync('sites/'+bkey+'/hyperconfig.json', JSON.stringify({key:password, target:address}));
+        fs.writeFileSync('sites/'+bkey+'/config.json', JSON.stringify({sites:[{subject:bkey+".matic.ml"}], maintainerEmail:email}));
+        fs.writeFileSync('address', bkey+".matic.ml");
+        rl.close();
+      })
     })
-  })
-});
+  });
+}
+
 
 rl.on('close', function () {
-  console.log('\nBYE BYE !!!');
   process.exit(0);
 });

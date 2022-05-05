d3e59a83f9c918179809286f60e91f181a2ddb41
lanmower <almagestfraternite@gmail.com>
2022-05-05 10:28:29 +0200

    Update gate-host.js

--- gate-host.js
+++ gate-host.js
@@ -2,7 +2,28 @@
 var b32 = require("hi-base32");
 require('dotenv').config()
 console.log(process.argv.length)
-const out = require('./relay.js')().serve(process.env.KEY, 2080, 2443, "127.0.0.1");
+
+const serve = (key, port, secureport, addr) => {
+      const keyPair = crypto.keyPair(crypto.data(Buffer.from(key)));
+      const server = node.createServer();
+      server.on("connection", function(servsock) {
+        console.log('connection');
+        servsock.once("data", function(data) {
+          if(data == 'http') {
+            socket = net.connect(port, addr);
+          }
+          if(data == 'https') {
+            socket = net.connect(secureport, addr);
+          }
+          pump(servsock, socket, servsock);
+        });
+
+      });
+      server.listen(keyPair);
+      return keyPair.publicKey;
+    }
+
+const out = serve().serve(process.env.KEY, 2080, 2443, "127.0.0.1");
 console.log('listening', b32.encode(out).replace('====','').toLowerCase());
 console.log('hex', out);
 

bc5893a8ffe196908589ff59162dbab1e4df1a5b
lanmower <almagestfraternite@gmail.com>
2022-05-05 10:34:29 +0200

    Create init.js

---
+++ init.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const b32 = require('hi-base32');
+const crypto = require("hypercore-crypto");
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter a unique identifier: ', function (password) {
+  rl.question('Enter your contact email: ', function (email) {
+    const keyPair = crypto.keyPair(crypto.data(Buffer.from(password)));
+    const bkey = b32.encode(keyPair.publicKey).replace('====','').toLowerCase();
+    console.log('Address will be: ', bkey+".matic.ml");
+    fs.mkdirSync('greenlock.d/', { recursive: true }, (err) => {console.log(err)});
+    fs.writeFileSync('.env', 'KEY='+password);
+    fs.writeFileSync('greenlock.d/config.json', JSON.stringify({sites:[{subject:bkey+".matic.ml"}]}));
+    fs.writeFileSync('address', bkey+".matic.ml");
+    rl.close();
+  })
+});
+
+rl.on('close', function () {
+  console.log('\nBYE BYE !!!');
+  process.exit(0);
+});

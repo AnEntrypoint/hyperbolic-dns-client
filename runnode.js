646fa30bc9c053ecddab217b14632dcc58040a71
lanmower <almagestfraternite@gmail.com>
2022-05-05 11:42:53 +0200

    Create runnode.js

---
+++ runnode.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const node = require('./node.js');
+const sites = fs.readDirSync('sites');
+for(const site of sites) {
+   const file = fs.readFileSync('./sites'+site+'/hyperconfig.json');
+   const config = JSON.parse(file);
+   const {key, target} = file;
+   node(key, target);
+}

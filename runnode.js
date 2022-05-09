a4c5d04ea5fb12919d72e9c8a3e0ee17b39143d4
lanmower <almagestfraternite@gmail.com>
2022-05-09 12:33:54 +0200

    Update runnode.js

--- runnode.js
+++ runnode.js
@@ -5,16 +5,8 @@
       await require('./init.js')();
       fs.closeSync(fs.openSync('run', 'w'));
    }
-   let file;
-   try {
-      file = fs.readFileSync('./site/hyperconfig.json');
-      routerfile = fs.readFileSync('./site/routerconfig.json');
-   } catch(e) {}
-   if(file) {
-      node( JSON.parse(file).key, process.env.target, process.env.http, process.env.https, JSON.parse(routerfile)); 
-   } else {
-      node( null, process.env.target, process.env.http, process.env.https, JSON.parse(routerfile) );
-   }
+   routerfile = fs.readFileSync('./site/routerconfig.json');
+   node(process.env.target, process.env.http, process.env.https, JSON.parse(routerfile)); 
 })()
 
 

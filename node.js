a5f9c919c920fb9f3291ffd4fb185d70bd3fe52b
lanmower <almagestfraternite@gmail.com>
2022-05-10 19:01:56 +0200

    Update node.js

--- node.js
+++ node.js
@@ -10,95 +10,94 @@ const node = new DHT({});
 
 require('dotenv').config()
 
-module.exports = ()=>{
-    const hyperconfig = JSON.parse(fs.readFileSync('./site/hyperconfig.json'));
-    console.log(hyperconfig);
-	
-    if(hyperconfig.name) await node.announce(DHT.hash(Buffer.from(hyperconfig.name), keyPair));
-	
-    const app = express()
-    let sslport = hyperconfig.https;
-    const { createProxyMiddleware } = require('http-proxy-middleware');
-    
-    const router = JSON.parse(fs.readFileSync('./site/routerconfig.json'));
-    const proxy = createProxyMiddleware({
-      router,
-      changeOrigin: true,
-      ws: true
-    });
-    app.use('/', proxy);
-    require("greenlock-express")
-      .init({
-        packageRoot: __dirname,
-        configDir: "./site/",
-        maintainerEmail: "jon@example.com",
-        cluster: false
+module.exports = () => {
+  const hyperconfig = JSON.parse(fs.readFileSync('./site/hyperconfig.json'));
+  console.log(hyperconfig);
+
+  
+  const app = express()
+  const { createProxyMiddleware } = require('http-proxy-middleware');
+
+  const router = JSON.parse(fs.readFileSync('./site/routerconfig.json'));
+  const proxy = createProxyMiddleware({
+    router,
+    changeOrigin: true,
+    ws: true
+  });
+  app.use('/', proxy);
+  require("greenlock-express")
+    .init({
+      packageRoot: __dirname,
+      configDir: "./site/",
+      maintainerEmail: "jon@example.com",
+      cluster: false
     }).ready(httpsWorker);
-    async function httpsWorker(glx) {
-      let https = 0;
-      let http = 0;
-      for(let conf of hyperconfig) {
-	let port = conf.http;
-	      let sslport = conf.https;
-      const done = ()=>{
-                const key = conf.key
-                const keyPair = crypto.keyPair(crypto.data(Buffer.from(key)));
-                const b32pub = b32.encode(keyPair.publicKey).replace('====','').toLowerCase();
-                const server = node.createServer();
-                server.on("connection", function(incoming) {
-                  incoming.once("data", function(data) {
-                    let outgoing;
-                    if(data == 'http') {
-                      outgoing = net.connect(http, '127.0.0.1');
-                    }
-                    if(data == 'https') {
-                      outgoing = net.connect(https, '127.0.0.1');
-                    }
-                    pump(incoming, outgoing, incoming);
-                  });
-                });
-                server.listen(keyPair);
-                console.log('listening', b32pub);
-                console.log('listening on https '+https);
-                console.log('listening on http '+http);
+  async function httpsWorker(glx) {
+    let https = 0;
+    let http = 0;
+    for (let conf of hyperconfig) {
+      if(conf.announce) await node.announce(DHT.hash(Buffer.from(conf.announce), keyPair));
+      let port = conf.http;
+      let sslport = conf.https;
+      const done = () => {
+        const key = conf.key
+        const keyPair = crypto.keyPair(crypto.data(Buffer.from(key)));
+        const b32pub = b32.encode(keyPair.publicKey).replace('====', '').toLowerCase();
+        const server = node.createServer();
+        server.on("connection", function (incoming) {
+          incoming.once("data", function (data) {
+            let outgoing;
+            if (data == 'http') {
+              outgoing = net.connect(http, '127.0.0.1');
+            }
+            if (data == 'https') {
+              outgoing = net.connect(https, '127.0.0.1');
+            }
+            pump(incoming, outgoing, incoming);
+          });
+        });
+        server.listen(keyPair);
+        console.log('listening', b32pub);
+        console.log('listening on https ' + https);
+        console.log('listening on http ' + http);
       }
       var httpsServer;
-      while(!https) {
+      while (!https) {
         try {
-              httpsServer = glx.httpsServer(null, app);
-              console.log('starting https', sslport);
-              await (new Promise((res)=>{
-                  httpsServer.listen(sslport, "0.0.0.0", function() {
-                      https = sslport;
-                      if(http && https) done();
-                      res();
-                  });
-              }))
-        } catch(e) {
-          sslport = 10240+parseInt(Math.random()*10240);
+          httpsServer = glx.httpsServer(null, app);
+          console.log('starting https', sslport);
+          await (new Promise((res) => {
+            httpsServer.listen(sslport, "0.0.0.0", function () {
+              https = sslport;
+              if (http && https) done();
+              res();
+            });
+          }))
+        } catch (e) {
+          sslport = 10240 + parseInt(Math.random() * 10240);
           console.error(e);
         }
-        await new Promise(res=>{setTimeout(res, 1000)});
+        await new Promise(res => { setTimeout(res, 1000) });
       }
       var httpServer;
-      while(!http) {
+      while (!http) {
         try {
           httpServer = glx.httpServer();
           console.log('starting http', port);
-          await (new Promise((res)=>{
-              httpServer.listen(port, "0.0.0.0", function() {
-                  http = port;
-                  if(http && https) done();
-                  res();
-              });
+          await (new Promise((res) => {
+            httpServer.listen(port, "0.0.0.0", function () {
+              http = port;
+              if (http && https) done();
+              res();
+            });
           }))
-        } catch(e) {
-            port = 10240+parseInt(Math.random()*10240);
-            console.error(e);
+        } catch (e) {
+          port = 10240 + parseInt(Math.random() * 10240);
+          console.error(e);
         }
-        await new Promise(res=>{setTimeout(res, 1000)});
-      }
+        await new Promise(res => { setTimeout(res, 1000) });
       }
     }
+  }
 }
 

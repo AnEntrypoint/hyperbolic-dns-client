e8e9324715152e945662521917b83af643e39137
lanmower <almagestfraternite@gmail.com>
2022-05-10 12:19:22 +0200

    Update node.js

--- node.js
+++ node.js
@@ -61,7 +61,7 @@ module.exports = ()=>{
       while(!https) {
         try {
               console.log('starting https', sslport);
-              while(await tcpPortUsed.check(port)) port = 10240+parseInt(Math.random()*10240);
+              while(tcpPortUsed.check(port)) port = 10240+parseInt(Math.random()*10240);
               await (new Promise((res)=>{
                   httpsServer.listen(sslport, "0.0.0.0", function() {
                       https = sslport;
@@ -71,7 +71,8 @@ module.exports = ()=>{
                   });
               }))
         } catch(e) {
-            console.error(e);
+          port = 10240+parseInt(Math.random()*10240);
+          console.error(e);
         }
         await new Promise(res=>{setTimeout(res, 1000)});
       }
@@ -79,7 +80,7 @@ module.exports = ()=>{
       while(!http) {
         try {
           console.log('starting http', port);
-          while(await tcpPortUsed.check(port)) port = 10240+parseInt(Math.random()*10240);
+          while(tcpPortUsed.check(port)) port = 10240+parseInt(Math.random()*10240);
           await (new Promise((res)=>{
               httpServer.listen(port, "0.0.0.0", function() {
                   http = port;
@@ -89,6 +90,7 @@ module.exports = ()=>{
               });
           }))
         } catch(e) {
+            port = 10240+parseInt(Math.random()*10240);
             console.error(e);
         }
         await new Promise(res=>{setTimeout(res, 1000)});

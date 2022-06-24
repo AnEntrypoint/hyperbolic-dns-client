c59beef7be3b9903d11657f0e425b0051318aa5c
lanmower <almagestfraternite@gmail.com>
2022-06-24 10:32:54 +0200

    Update init.js

--- init.js
+++ init.js
@@ -8,12 +8,12 @@ const run = (email, address)=>{
   const bkey = b32.encode(keyPair.publicKey).replace('====','').toLowerCase();
   console.log('Address will be: ', bkey+".sites.247420.xyz");
   fs.mkdirSync('site/', { recursive: true }, (err) => {console.log(err)});
-  fs.writeFileSync('site/hyperconfig.json', JSON.stringify([process.env.domainname]));
+  fs.writeFileSync('../hyperconfig.json', JSON.stringify([process.env.domainname]));
   fs.writeFileSync('site/config.json', JSON.stringify({sites:[{subject:bkey+".sites.247420.xyz"},{subject:process.env.domainname+".sites.247420.xyz"}], defaults:{subscriberEmail:email}}));
   const router = {};
   router[bkey+".sites.247420.xyz"] = "http://localhost:8080";
   router[process.env.domainname+".sites.247420.xyz"] = "http://localhost:8080";
-  fs.writeFileSync('site/routerconfig.json', JSON.stringify(router));
+  fs.writeFileSync('../routerconfig.json', JSON.stringify(router));
   fs.writeFileSync('address', bkey+".sites.247420.xyz");
 }
 
